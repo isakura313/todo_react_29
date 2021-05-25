fe4989a0bc7960d24fd11f29c1c0bf0630adceaa
doshen.vv <vasilisa_chehova@mail.ru>
2021-05-25 10:52:03 +0300

    fifth_stage

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxhyGyH2X4SLVSkrmva9Bvhw3GLl71gh4",
+    authDomain: "todo-85c4f.firebaseapp.com",
+    projectId: "todo-85c4f",
+    storageBucket: "todo-85c4f.appspot.com",
+    messagingSenderId: "856861952347",
+    appId: "1:856861952347:web:ea17e49f0677caa6e7f27e"
+  };
